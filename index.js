45395a264ed70aa8ec779e4cafe3e2e283cbf121
Izay0i <47558555+Izay0i@users.noreply.github.com>
2021-06-24 08:32:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const path = require('path');
 const history = require('connect-history-api-fallback');
 const app = express();
-const port = process.env.PORT || 443;
+const port = process.env.PORT || 80;
 
 const authRouter = require('./routes/auth');
 const userRouter = require('./routes/user');
